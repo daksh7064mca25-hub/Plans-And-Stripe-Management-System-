ebf70d56291fb48818e538cca070446dc850a2b3
Daksh <daksh7064.mca25@chitkara.edu.in>
2026-08-09 21:06:44 +0530

    backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://plans-and-stripe-management-system.onrender.com',
   withCredentials: true, // Crucial for HTTP-only cookies
 });
 
